9746b37755ea0e355cae9a7ff52ca38fa01d23f9
JonathanVialpando <109265169+JonathanVialpando@users.noreply.github.com>
2024-05-29 21:29:43 -0700

    updates

--- main.py
+++ main.py
@@ -12,12 +12,15 @@ def home():
         
         if form == "generate":
             strength = request.form.get("strength")
+
             if strength == "weak":
                 finalPassword = generatePassword(1)
                 return render_template('resultsGenerate.html', password=finalPassword)
+            
             if strength == "medium":
                 finalPassword = generatePassword(2)
                 return render_template('resultsGenerate.html', password=finalPassword)
+            
             if strength == "strong":
                 finalPassword = generatePassword(3)
                 return render_template('resultsGenerate.html', password=finalPassword)
@@ -28,36 +31,43 @@ def home():
             return render_template('resultsCheck.html', finalMSG=finalMSG)
 
     return render_template('home.html')
-#def chechPassword(password):
-    # if password.
 
 def generatePassword(strength):
     passwordList = string.ascii_lowercase
+
     if strength == 1:
         finalPassword = ''.join(random.choice(passwordList) for i in range(8))
         return finalPassword
+    
     elif strength == 2:
         passwordList += string.ascii_uppercase
         finalPassword = ''.join(random.choice(passwordList) for i in range(10))
         return finalPassword
+    
     elif strength == 3:
-        passwordList += string.digits + string.ascii_uppercase
+        passwordList += string.digits + string.ascii_uppercase + string.punctuation
         finalPassword = ''.join(random.choice(passwordList) for i in range(15))
         return finalPassword
 
 def checkPassword(password):
     msg = ""
+
     if password == "":
         msg += "Your password is VERY weak, try to use some characters"
         return msg
+    
     if len(password) < 8: 
-        msg += "Try adding more characters <br>"
+        msg += "Try adding more characters <br><br>"
+
     if password.islower() :
-        msg += "Try using capital characters <br>"
+        msg += "Try using capital characters <br><br>"
+
     if any(char.isdigit() for char in password) == 0:
-        msg += "Try using a digit <br>"
+        msg += "Try using a digit <br><br>"
+
     if msg == "":
         msg += "Your password is Strong!"
+        
     return msg 
 
 if __name__ == '__main__':
